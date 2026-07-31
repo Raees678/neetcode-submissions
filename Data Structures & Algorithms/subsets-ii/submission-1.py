104a3ba5ce8ae6912b1791548e646abfd3a6cbb1
Raees Rajwani <raeesrajwani95@gmail.com>
2026-07-31 05:47:57 +0200

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        subsets = [[]]
+        counter = Counter(nums)
+        nums_count = [(num, count) for num, count in counter.items()]
+        def helper(i):
+            if i >= len(nums_count):
+                return
+            
+            num, count = nums_count[i]
+
+            new_subsets = []
+            for s in subsets:
+                val = []
+                for c in range(count):
+                    val.append(num)
+                    s_copy = s.copy()
+                    s_copy.extend(val.copy())
+                    new_subsets.append(s_copy)
+            
+            subsets.extend(new_subsets)
+
+            helper(i+1)
+        
+        helper(0)
+
+        return subsets
+        
